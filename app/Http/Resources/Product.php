56d978c9b07f61a4687e92f708fdeb8548aa25a3
Oriana <ori@oriana.com>
2021-04-11 18:22:54 -0400

    Implementando HATEOAS

--- app/Http/Resources/Product.php
+++ app/Http/Resources/Product.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\User;
+use App\Http\Resources\User as UserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Product extends JsonResource
@@ -22,10 +23,34 @@ class Product extends JsonResource
             'quantity' => (int)$this->quantity,
             'status' => (string)$this->status,
             'image' => url('img/{$user->image}'),
-            'buyer' => User::find($this->user_id),
+            'buyer' => new UserResource(User::find($this->user_id)),
             'created_at' => (string)$this->created_at,
             'updated_at' => (string)$this->updated_at,
-            'deleted_at' => (string)$this->deleted_at
+            'deleted_at' => (string)$this->deleted_at,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('products.show',$this->id),
+                ],
+                [
+                    'rel' => 'products.buyers',
+                    'href' => route('products.buyers.index',$this->id),
+                ],
+                [
+                    'rel' => 'products.categories',
+                    'href' => route('products.categories.index',$this->id),
+                ],
+                [
+                    'rel' => 'products.transactions',
+                    'href' => route('products.transactions.index',$this->id),
+                ],
+                [
+                    'rel' => 'seller',
+                    'href' => route('sellers.show',$this->user_id),
+                ],
+            ],            
+
         ];
     }
 }
--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -22,7 +22,48 @@ class User extends JsonResource
             'verification_token' => (string) $this->verification_token,
             'created_at' => (string)$this->created_at,
             'updated_at' => (string)$this->updated_at,
-            'deleted_at' => (string)$this->deleted_at
+            'deleted_at' => (string)$this->deleted_at,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('users.show',$this->id),
+                ],
+                //buyer
+                [
+                    'rel' => 'buyers.categories',
+                    'href' => route('buyers.categories.index',$this->id),
+                ],
+                [
+                    'rel' => 'buyers.products',
+                    'href' => route('buyers.products.index',$this->id),
+                ],
+                [
+                    'rel' => 'buyers.sellers',
+                    'href' => route('buyers.sellers.index',$this->id),
+                ],
+                [
+                    'rel' => 'buyers.transactions',
+                    'href' => route('buyers.transactions.index',$this->id),
+                ],
+                //seller
+                [
+                    'rel' => 'sellers.buyers',
+                    'href' => route('sellers.buyers.index',$this->id),
+                ],
+                [
+                    'rel' => 'sellers.categories',
+                    'href' => route('sellers.categories.index',$this->id),
+                ],
+                [
+                    'rel' => 'sellers.products',
+                    'href' => route('sellers.products.index',$this->id),
+                ],
+                [
+                    'rel' => 'sellers.transactions',
+                    'href' => route('sellers.transactions.index',$this->id),
+                ],
+            ],
         ];
     }
 }
